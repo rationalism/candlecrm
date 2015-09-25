fb4b79e982d0d404a7f06d77332de8ce1ac3f322
Alyssa Vance <alyssamvance@gmail.com>
2015-09-25 02:46:00 -0700

    deeplearning4j files

---
+++ src/spectra/deepnet.clj
@@ -0,0 +1,6 @@
+(ns spectra.deepnet
+  (:require [clojure.java.io :as io]
+            [environ.core :refer [env]]
+            [taoensso.timbre.profiling :as profiling
+             :refer (pspy pspy* profile defnp p p*)])
+  (:import))
---
+++ test/spectra/deepnet_test.clj
@@ -0,0 +1,6 @@
+(ns spectra.deepnet-test
+  (:require [clojure.test :refer :all]
+            [spectra.deepnet :refer :all]))
+
+(deftest first-test
+  (is "Tests should be written"))
