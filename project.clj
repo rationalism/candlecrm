8e73c421cd0fcf7dd878d674325abeec6a60cd9a
Alyssa Vance <alyssamvance@gmail.com>
2015-09-25 20:12:11 -0700

    larger java heap

--- project.clj
+++ project.clj
@@ -38,4 +38,5 @@
          :init spectra.web/app-init
          :destroy spectra.web/app-shutdown
          :nrepl {:start? true :port 9998}}
+  :jvm-opts ["-Xmx2g"]
   :uberjar-name "spectra-standalone.jar")
