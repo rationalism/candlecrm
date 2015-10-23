32c6417bf16b98bc446ca856a3ec7b4062b6cefc
Alyssa Vance <alyssamvance@gmail.com>
2015-10-22 20:30:29 -0700

    update to neo4j test

--- test/spectra/neo4j_test.clj
+++ test/spectra/neo4j_test.clj
@@ -1,7 +1,7 @@
 (ns spectra.neo4j-test
   (:require [clojure.test :refer :all]
             [spectra.neo4j :refer :all]
-            [spectra.schema :as schema]))
+            [spectra.schema :as s]))
 
 (defn graph-ready [f]
   (intern 'spectra.neo4j '*graph* (get-graph))
@@ -24,29 +24,29 @@
 
 (deftest vertex-create-destroy
   (testing "create, find, destroy vertices"
-    (def get-people (partial get-vertices-coll schema/person-type))
-    (def test-name {schema/name-type (first test-names)})
-    (def test-email {schema/email-address-type (first test-emails)})
-    (def test-phone {schema/phone-num-type (first test-phones)})
+    (def get-people (partial get-vertices-coll s/person))
+    (def test-name {s/name (first test-names)})
+    (def test-email {s/email-addr (first test-emails)})
+    (def test-phone {s/phone-num (first test-phones)})
     (def test-fake {fake-prop (first test-fake-prop)})
     (is (= 0 (count (get-people test-name))))
     (is (= 0 (count (get-people test-email))))
     (is (= 0 (count (get-people test-phone))))
     
     (def new-vertex
-      (create-vertex! schema/person-type
-                      {schema/name-type test-names
-                       schema/email-address-type test-emails
-                       schema/phone-num-type test-phones
+      (create-vertex! s/person
+                      {s/name test-names
+                       s/email-addr test-emails
+                       s/phone-num test-phones
                        fake-prop test-fake-prop}))
     
     (is (= 1 (count (get-people test-name))))
     (is (= 1 (count (get-people test-email))))
     (is (= 1 (count (get-people test-phone))))
     (is (= 0 (count (get-people test-fake))))
-    (is (= 0 (count (get-people {schema/name-type other-name}))))
-    (is (= 0 (count (get-people {schema/email-address-type other-email}))))
-    (is (= 0 (count (get-people {schema/phone-num-type other-phone}))))
+    (is (= 0 (count (get-people {s/name other-name}))))
+    (is (= 0 (count (get-people {s/email-addr other-email}))))
+    (is (= 0 (count (get-people {s/phone-num other-phone}))))
     
     (is (= 1 (count (get-people (merge test-name test-email test-phone)))))
 
@@ -58,19 +58,16 @@
 (deftest recon-property
   (testing "Reconcile old values of a property with new values"
     (def new-vertex
-      (create-vertex! schema/person-type
-                      {schema/name-type test-names}))
+      (create-vertex! s/person
+                      {s/name test-names}))
     (is (= test-names
-           (get-property new-vertex schema/name-type)))
-    
-    (recon-property-list! new-vertex schema/name-type (first test-names))
+           (get-property new-vertex s/name)))
+    (recon-property-list! new-vertex s/name (first test-names))
     (def new-vertex (refresh-vertex new-vertex))
     (is (= test-names
-           (get-property new-vertex schema/name-type)))
-    
-    (recon-property-list! new-vertex schema/name-type other-name)
+           (get-property new-vertex s/name)))
+    (recon-property-list! new-vertex s/name other-name)
     (def new-vertex (refresh-vertex new-vertex))
     (is (= (conj test-names other-name)
-           (get-property new-vertex schema/name-type)))
-
+           (get-property new-vertex s/name)))
     (delete-vertex! new-vertex)))
