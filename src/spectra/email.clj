8a0849b9cf1b99d43c8a308c8d4c46f61aaa83d9
Alyssa Vance <alyssamvance@gmail.com>
2015-12-04 02:25:54 -0800

    exact bin search bugfix

--- src/spectra/email.clj
+++ src/spectra/email.clj
@@ -65,17 +65,29 @@
 (defn sent-time [message]
   (.getSentDate message))
 
+(defn min-loc [nums]
+  (apply min-key #(get (into [] nums) %)
+         (range (count nums))))
+
+(defn exact-num [folder time nums]
+  (->> (map #(get-message folder %) nums)
+       (map sent-time) (map dt/to-ms)
+       (map #(- % time)) (map #(Math/abs %))
+       min-loc (get nums)))
+
 (defn find-num
   ([folder time]
    (find-num folder time 0 (message-count folder)))
   ([folder time bottom top]
-   (if (or (= bottom top) (= bottom (dec top)))
-     bottom
-     (let [mid (quot (+ top bottom) 2)]
-       (if (->> mid (get-message folder)
-                sent-time dt/to-ms (< time))
-         (recur folder time bottom mid)
-         (recur folder time mid top))))))
+   (cond (= bottom top) bottom
+         (= bottom (dec top))
+         (exact-num folder time [bottom (inc bottom)])
+         :else 
+         (let [mid (quot (+ top bottom) 2)]
+           (if (->> mid (get-message folder)
+                    sent-time dt/to-ms (< time))
+             (recur folder time bottom mid)
+             (recur folder time mid top))))))
 
 (defonce imap-lookup (atom {}))
 
