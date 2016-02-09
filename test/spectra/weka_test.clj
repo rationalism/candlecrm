f2f41f995c9675179e6668346dc7f37a32a13ffd
Alyssa Vance <alyssamvance@gmail.com>
2016-02-08 23:30:01 -0800

    add weka tests

--- test/spectra/weka_test.clj
+++ test/spectra/weka_test.clj
@@ -2,5 +2,18 @@
   (:require [clojure.test :refer :all]
             [spectra.weka :refer :all]))
 
-(deftest first-test
-  (is "Tests should be written"))
+(def train-points [[0.0 0.0 0.0 0.0]
+                   [0.0 0.0 0.0 0.0]
+                   [0.0 0.0 0.0 0.0]
+                   [0.0 0.0 0.0 0.0]
+                   [1.0 1.0 1.0 1.0]
+                   [1.0 1.0 1.0 1.0]
+                   [1.0 1.0 1.0 1.0]
+                   [1.0 1.0 1.0 1.0]])
+
+(deftest train-and-use
+  (testing "Train and use a random forest classifier"
+    (def forest (make-forest 100 train-points))
+    (is forest)
+    (is (= 0.0 (classify forest [0.0 0.0 0.0])))
+    (is (= 1.0 (classify forest [1.0 1.0 1.0])))))
