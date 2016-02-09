c1dc6ec9201bffcc2b02993f3b7b475f2e3a6320
Alyssa Vance <alyssamvance@gmail.com>
2016-02-08 23:38:26 -0800

    web test

--- test/spectra/web_test.clj
+++ test/spectra/web_test.clj
@@ -1,6 +1,9 @@
 (ns spectra.web-test
   (:require [clojure.test :refer :all]
+            [ring.middleware.defaults :refer :all]
             [spectra.web :refer :all]))
 
-(deftest first-test
-  (is "Tests should be written"))
+(deftest app-exists
+  (testing "The app exists"
+    (is (wrap-defaults (friend-authenticate app)
+                       (middleware-config)))))
