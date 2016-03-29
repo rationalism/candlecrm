c381473b05b2bde1cdce5bd6d045a8483c77bba6
Alyssa Vance <alyssamvance@gmail.com>
2016-03-29 02:03:35 -0700

    schedule contacts

--- src/spectra/quartz.clj
+++ src/spectra/quartz.clj
@@ -191,7 +191,7 @@
   (reset! scheduler (qs/start (qs/initialize)))
   (qs/schedule @scheduler
                (make-job EmailLoad "jobs.email.load.1")
-               (periodic-trigger 2000 nil "email.trigger.1"))
+               (periodic-trigger 5000 nil "email.trigger.1"))
   (qs/schedule @scheduler
                (make-job EmailRefresh "jobs.email.load.2")
                (periodic-trigger 3600000 nil "email.trigger.2"))
