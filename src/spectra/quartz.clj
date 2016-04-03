fe80a772c20563f4899a27d70c17bd4c7050b074
Alyssa Vance <alyssamvance@gmail.com>
2016-04-03 01:35:25 -0700

    extend recon time limit

--- src/spectra/quartz.clj
+++ src/spectra/quartz.clj
@@ -222,7 +222,7 @@
                (periodic-trigger 5000 nil "geocode.trigger.2"))
   (qs/schedule @scheduler
                (make-job ProcessRecon "jobs.recon.do.1")
-               (periodic-trigger 5000 nil "recon.trigger.1"))
+               (periodic-trigger 10000 nil "recon.trigger.1"))
   (qs/schedule @scheduler
                (make-job EmailNLP "jobs.nlp.email.1")
                (periodic-trigger 2000 nil "nlp.trigger.1"))
