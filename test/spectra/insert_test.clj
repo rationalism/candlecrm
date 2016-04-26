6fec2d78a61c01aee8f105b10f88db91797d9f3c
Alyssa Vance <alyssamvance@gmail.com>
2016-04-26 01:30:18 -0700

    add insert test

--- test/spectra/insert_test.clj
+++ test/spectra/insert_test.clj
@@ -17,11 +17,14 @@
 
 (def node1 {:label :fish :species "tuna"})
 (def node2 {:label :phylum :phy-name "Vertebrates"})
+(def query (str "MATCH (a)-[r:`has-phylum` {prov_test:1}]->(b)"
+                " RETURN r"))
 
 (deftest insert-destroy
   (testing "Insert and destroy some nodes"
     (def g (loom/build-graph [node1 node2]
-                             [[node1 node2 :has-phylum]]))
+                             [[node1 node2 {:label :has-phylum
+                                            :prov_test 1}]]))
 
     (def user (auth/create-user! {:username test-username
                                   :password test-password}))
@@ -29,12 +32,14 @@
     (push-graph! g user)
     (is (= 1 (count (neo4j/get-vertices user :fish {:species "tuna"}))))
     (is (= 1 (count (neo4j/get-vertices user :phylum {:phy-name "Vertebrates"}))))
+    (is (= 1 (count (neo4j/cypher-query-raw query))))
 
     (neo4j/delete-vertex! (first (neo4j/get-vertices user :fish {:species "tuna"})))
     (neo4j/delete-vertex! (first (neo4j/get-vertices user :phylum {:phy-name "Vertebrates"})))
     
     (is (= 0 (count (neo4j/get-vertices user :fish {:species "tuna"}))))
     (is (= 0 (count (neo4j/get-vertices user :phylum {:phy-name "Vertebrates"}))))
+    (is (= 0 (count (neo4j/cypher-query-raw query))))
     
     (auth/delete-user! (auth/lookup-user test-username))))
 
