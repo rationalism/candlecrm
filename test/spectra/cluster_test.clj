afd6ef5f6871732857982044b6c7bf8c3d860bf1
Alyssa Vance <alyssamvance@gmail.com>
2016-05-03 17:54:07 -0700

    cluster test

--- test/spectra/cluster_test.clj
+++ test/spectra/cluster_test.clj
@@ -1,6 +1,8 @@
 (ns spectra.cluster-test
   (:require [clojure.test :refer :all]
+            [spectra.loom :as loom]
             [spectra.cluster :refer :all]))
 
-(deftest first-test
-  (is "Tests should be written"))
+(deftest make-random
+  (testing "Generate a random graph"
+    (is (-> 10 random-graph loom/subgraphs count (= 1)))))
