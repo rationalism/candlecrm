01ebce475ae9e63d097f272b671d26c7b41172b4
Alyssa Vance <alyssamvance@gmail.com>
2016-05-05 10:46:28 -0700

    geocode test

--- test/spectra/geocode_test.clj
+++ test/spectra/geocode_test.clj
@@ -2,5 +2,6 @@
   (:require [clojure.test :refer :all]
             [spectra.geocode :refer :all]))
 
-(deftest first-test
-  (is "Tests should be written"))
+(deftest fetch-geocodes
+  (testing "Try getting some geocodes"
+    (is (geocode-cached 10))))
