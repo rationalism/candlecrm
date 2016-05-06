917b6e4a0f3826a5e2ce1ae4d19c92e5fc152163
Alyssa Vance <alyssamvance@gmail.com>
2016-05-05 18:21:12 -0700

    csrf test

--- test/spectra/web_test.clj
+++ test/spectra/web_test.clj
@@ -9,3 +9,16 @@
     (is (wrap-defaults
          (wrap-authentication app)
          (middleware-config)))))
+
+(deftest csrf-validation
+  (testing "CSRF tokens are findable"
+    (def csrf-1 {:form-params {"__anti-forgery-token" "sometoken"}})
+    (def csrf-2 {:multipart-params {"__anti-forgery-token" "sometoken"}})
+    (def csrf-3 {:headers {"x-csrf-token" "sometoken"}})
+    (def csrf-4 {:headers {"x-xsrf-token" "sometoken"}})
+    (def csrf-5 {:params {:csrf-token "sometoken"}})
+    (is (= "sometoken" (csrf-token csrf-1)))
+    (is (= "sometoken" (csrf-token csrf-2)))
+    (is (= "sometoken" (csrf-token csrf-3)))
+    (is (= "sometoken" (csrf-token csrf-4)))
+    (is (= "sometoken" (csrf-token csrf-5)))))
