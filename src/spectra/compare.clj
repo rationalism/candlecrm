9a9f066f29b1954160240377b92b030f60548128
Alyssa Vance <alyssamvance@gmail.com>
2016-05-13 22:04:20 -0700

    comparison operations

--- src/spectra/compare.clj
+++ src/spectra/compare.clj
@@ -1,6 +1,8 @@
 (ns spectra.compare
   (:require [clojure.string :as str]
-            [spectra.common :as com])
+            [spectra.common :as com]
+            [taoensso.timbre.profiling :as profiling
+             :refer (pspy pspy* profile defnp p p*)])
   (:import [org.apache.commons.math3.distribution NormalDistribution]))
 
 (def random-limit 21)
@@ -23,17 +25,57 @@
   (let [dist (NormalDistribution. mean sd)]
     (take n (repeatedly #(.sample dist)))))
 
+(defn estimate-init [candidates]
+  (->> (/ (- random-limit 1) 2)
+       (* pretty-mult) (repeat (count candidates))
+       (interleave candidates)
+       (partition 2) (map vec) vec))
+
 (defn test-candidates [n]
   (->> #(* pretty-mult (rand-int random-limit))
-       (repeatedly n)
-       (zipmap (range n))))
-
-(defn estimate-init [n]
-  (->> (/ (- random-limit 1) 2)
-       (* pretty-mult) (repeat n)
-       (zipmap (range n))))
+       (repeatedly n) (interleave (range n))
+       (partition 2)))
 
 (defn test-score [c1 c2]
   (-> (second c2) (- (second c1))
       (gaussian-sample pretty-mult 1)
       first logit))
+
+(defn adjust-size [cs i cmp]
+  (-> (cmp (first (nth cs i))
+           (first (nth cs (inc i))))
+      inverse-logit
+      (- (second (nth cs (inc i))))
+      (+ (second (nth cs i)))
+      (/ 2.0)))
+
+(defn adjust-score [adj]
+  (fn [score]
+    (update score 1 #(+ % adj))))
+
+(defn sort-second [coll]
+  (sort-by second coll))
+
+(defn adjust-scores [cs i cmp]
+  (let [adj (adjust-size cs i cmp)]
+    (-> (update cs i (adjust-score (- adj)))
+        (update (inc i) (adjust-score adj))
+        sort-second vec)))
+
+(defn pair-selected? [c1 c2]
+  (-> (second c2) (- (second c1)) Math/abs
+      (- pretty-mult) Math/abs (* -1)
+      (gaussian-sample pretty-mult 1)
+      first pos?))
+
+(defn estimate-scores [candidates cmp]
+  (let [c (count candidates)
+        n (-> c Math/log Math/ceil int (* (dec c)))]
+    (loop [scores (estimate-init candidates)
+           i 0 cnt 0]
+      (if (= cnt n) scores
+          (if (pair-selected? (nth scores i)
+                              (nth scores (inc i)))
+            (recur (adjust-scores scores i cmp)
+                   (mod (inc i) (dec c)) (inc cnt))
+            (recur scores (mod (inc i) (dec c)) cnt))))))
