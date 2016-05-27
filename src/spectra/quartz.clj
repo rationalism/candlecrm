85eb370c9b97fbff0842dcfa8014965a9e53c609
Alyssa Vance <alyssamvance@gmail.com>
2016-05-26 21:14:30 -0700

    activate scheduler

--- src/spectra/quartz.clj
+++ src/spectra/quartz.clj
@@ -15,7 +15,7 @@
             [spectra.email :as email]
             [spectra.geocode :as geocode]
             [spectra.google :as google]
-            [spectra.index :as index]
+            [spectra.index x:as index]
             [spectra.insert :as insert]
             [spectra.mlrecon :as mlrecon]
             [spectra.neo4j :as neo4j]
@@ -24,7 +24,7 @@
             [taoensso.timbre.profiling :as profiling
              :refer (pspy pspy* profile defnp p p*)]))
 
-(def nonlp-insert-limit 600000)
+(def nonlp-insert-limit 60)
 
 (defn message-count [user]
   (-> user email/fetch-imap-folder email/message-count))
@@ -127,7 +127,7 @@
 
 ;; Nils here allow for easy switching on/off
 (jobs/defjob EmailLoad [ctx]
-  (when nil (queue-pop!)))
+  (when :nil (queue-pop!)))
 
 (jobs/defjob NewGeocodes [ctx]
   (neo4j/thread-wrap (geocode/geocode-batch 10)))
@@ -136,10 +136,10 @@
   (neo4j/thread-wrap (geocode/geocode-cached 20)))
 
 (jobs/defjob ProcessRecon [ctx]
-  (when nil (run-recon!)))
+  (when :nil (run-recon!)))
 
 (jobs/defjob EmailNLP [ctx]
-  (neo4j/thread-wrap (when nil (email/push-email-nlp!))))
+  (neo4j/thread-wrap (when :nil (email/push-email-nlp!))))
 
 (jobs/defjob EmailRefresh [ctx]
   (neo4j/thread-wrap
@@ -221,7 +221,7 @@
                (periodic-trigger 5000 nil "geocode.trigger.2"))
   (qs/schedule @scheduler
                (make-job ProcessRecon "jobs.recon.do.1")
-               (periodic-trigger 10000 nil "recon.trigger.1"))
+               (periodic-trigger 5000 nil "recon.trigger.1"))
   (qs/schedule @scheduler
                (make-job EmailNLP "jobs.nlp.email.1")
                (periodic-trigger 10000 nil "nlp.trigger.1"))
