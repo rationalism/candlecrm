38f31e63c7036206571e90c71c7534503bd47a77
Alyssa Vance <alyssamvance@gmail.com>
2016-06-13 17:43:34 -0700

    kill test code

--- src/spectra/auth.clj
+++ src/spectra/auth.clj
@@ -44,12 +44,6 @@
     (-> token (jwe/decrypt privkey encryption)
         :user :id neo4j/find-by-id)))
 
-(defn make-id-token [id]
-  {:token
-   (jwe/encrypt {:user {:id id}
-                 :exp (-> exp-hours hours from-now)}
-                pubkey encryption)})
-
 (defn make-token [user]
   {:token
    (jwe/encrypt {:user {:id (.id user)}
