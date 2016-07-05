60d2de3bad6def410d3e5177503ae7100f893251
Alyssa Vance <alyssamvance@gmail.com>
2016-07-05 15:36:48 -0700

    remove cyclic dep

--- src/spectra/reply.clj
+++ src/spectra/reply.clj
@@ -1,7 +1,7 @@
 (ns spectra.reply
   (:require [clojure.string :as str]
             [spectra.common :refer :all]
-            [spectra.imap :as imap]))
+            [spectra.weka :as weka]))
 
 (defn add-2 [n]
   (+ 2 n))
