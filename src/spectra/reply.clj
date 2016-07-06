312269dc7b28580c3631f14065bcf2cdf06dcba2
Alyssa Vance <alyssamvance@gmail.com>
2016-07-05 17:46:25 -0700

    header ranges fn

--- src/spectra/reply.clj
+++ src/spectra/reply.clj
@@ -1,6 +1,7 @@
 (ns spectra.reply
   (:require [clojure.string :as str]
             [spectra.common :refer :all]
+            [spectra.corenlp :as nlp]
             [spectra.weka :as weka]))
 
 (defn count-arrows [lines]
@@ -12,3 +13,8 @@
     (if (or (nil? arrows) (empty? arrows))
       [0] (->> arrows (map first) (map count)))))
 
+(defn header-ranges [sep-model lines]
+  (->> lines count range (zipvec lines)
+       (mapvals #(weka/is-header? sep-model (first %)))
+       (into []) (partition-by second)
+       (filter #(-> % first second)) (map #(map first %))))
