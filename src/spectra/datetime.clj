4f053ce3c83f7cba727e0f3ef42bd8422b516063
Alyssa Vance <alyssamvance@gmail.com>
2016-07-10 14:30:08 -0700

    stuff

--- src/spectra/datetime.clj
+++ src/spectra/datetime.clj
@@ -42,6 +42,7 @@
          (not= (first parsed-date) (second parsed-date)))))
 
 (defn parse-dates-raw [text reference]
+  (throw-info! text)
   (CalendarSource/setBaseDate reference)
   (.parse (Parser. ) text))
 
