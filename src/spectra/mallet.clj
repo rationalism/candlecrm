43e6999c5b7111b0fdec9221cd9e7e8a2cc24ce4
Alyssa Vance <alyssamvance@gmail.com>
2016-07-11 17:38:59 -0700

    add mallet files

---
+++ src/spectra/mallet.clj
@@ -0,0 +1,5 @@
+(ns spectra.mallet
+  (:require [clojure.string :as str]
+            [spectra.common :refer :all]
+            [spectra_cljc.schema :as s]
+            [environ.core :refer [env]]))
---
+++ test/spectra/mallet_test.clj
@@ -0,0 +1,6 @@
+(ns spectra.mallet-test
+  (:require [clojure.test :refer :all]
+            [spectra.mallet :refer :all]))
+
+(deftest first-test
+  (is "Tests should be written"))
