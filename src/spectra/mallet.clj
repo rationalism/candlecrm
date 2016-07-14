95a131c26107585be10b8aa38819ad42eedbccd3
Alyssa Vance <alyssamvance@gmail.com>
2016-07-13 23:57:33 -0700

    split strings ourselves, don't trust Mallet

--- src/spectra/mallet.clj
+++ src/spectra/mallet.clj
@@ -13,31 +13,41 @@
             TokenSequenceNGrams]
            [cc.mallet.pipe.iterator ArrayIterator
             ArrayDataAndTargetIterator]
-           [cc.mallet.types InstanceList]))
+           [cc.mallet.types InstanceList TokenSequence]))
+
+(def char-regex #"[\s:.@]+")
 
 (defn pipe []
   (SerialPipes.
-   [(Target2Label.) (CharSequence2TokenSequence. "[^\\s:.@]+")
-    (TokenSequenceLowercase.)
+   [(Target2Label.) (TokenSequenceLowercase.)
     (TokenSequence2FeatureSequence.)
     (FeatureSequence2FeatureVector.)]))
 
 (defn average [coll]
   (/ (apply + coll) (count coll)))
 
+(defn add-token [sequence token]
+  (.add sequence token) sequence)
+
+(defnc token-sequence [line]
+  (reduce add-token (TokenSequence. )
+          (str/split line char-regex)))
+
 (defn make-instances [model lines]
   (doto (InstanceList. (.getInstancePipe model))
     (.addThruPipe (->> model .getLabelAlphabet .toArray first
                        (repeat (count lines))
-                       (ArrayDataAndTargetIterator. lines)))))
+                       (ArrayDataAndTargetIterator.
+                        (map token-sequence lines))))))
 
 (defn train-instances [lines]
   (doto (InstanceList. (pipe))
     (.addThruPipe (ArrayDataAndTargetIterator.
-                   (map first lines) (map second lines)))))
+                   (map token-sequence (map first lines))
+                   (map second lines)))))
 
 (defn split-instances [instances]
-  (->> [0.99 0.01] double-array
+  (->> [0.999 0.001] double-array
        (.split instances (java.util.Random.))))
 
 (defn file-instances [filename]
