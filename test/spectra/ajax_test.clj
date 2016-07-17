b950285a41469a2a1d4f08c7c15452fba52830ab
Alyssa Vance <alyssamvance@gmail.com>
2016-07-16 17:54:57 -0700

    test fixes

--- test/spectra/ajax_test.clj
+++ test/spectra/ajax_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [spectra.ajax :refer :all]))
 
+(defn graph-ready [f]
+  (neo4j/graph-connect!)
+  (neo4j/reset-session!)
+  (f)
+  (neo4j/close-session!))
+
+(use-fixtures :once graph-ready)
+
 (deftest router-broadcaster
   (testing "Start and stop of Sente server"
     (start-router!)
