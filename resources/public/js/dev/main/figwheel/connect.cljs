e824812170597a00a46969adbe2c2cbdcff8f80f
Alyssa Vance <alyssamvance@gmail.com>
2016-07-17 18:30:50 -0700

    update gitignore

--- resources/public/js/dev/main/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [spectra_cljs.init] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:websocket-url "wss://www.candlecrm.com:3450/figwheel-ws", :build-id "dev"})
-
