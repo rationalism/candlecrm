a533472236468b05fecc6939fad263a0b89a8e78
Alyssa Vance <alyssamvance@gmail.com>
2016-07-17 22:27:52 -0700

    fix recon features

--- src/candlecrm/model.clj
+++ src/candlecrm/model.clj
@@ -174,7 +174,9 @@
    [s/s-name s/zipcode]
    s/event
    [s/start-time s/stop-time s/event-begin
-    s/event-end s/website]})
+    s/event-end s/website]
+   s/building
+   [s/street-addr]})
 
 (def scoring
   {s/email
@@ -201,21 +203,25 @@
     [[s/desc2] [is-eq lcs lev]]
     [[s/item-cost] [is-eq abs]]]
    s/location
-   [[[s/s-name] [is-eq lcs lev shortest]
-     [s/zipcode] [is-eq lcs lev shortest]
-     [s/located-in s/street-addr] [is-eq lcs lev shortest]
-     [s/loc-inside s/s-name] [overlap lcs lev shortest]
-     [s/link-to s/link-id] [overlap]]]
+   [[[s/s-name] [is-eq lcs lev shortest]]
+    [[s/zipcode] [is-eq lcs lev shortest]]
+    [[s/located-in s/street-addr] [is-eq lcs lev shortest]]
+    [[s/loc-inside s/s-name] [overlap lcs lev shortest]]
+    [[s/link-to s/link-id] [overlap]]]
    s/event
    [[[s/start-time] [is-eq lcs lev shortest]]
-    [[s/stop-time] [is-eq lcs lev shortest]
-     [s/event-begin] [is-eq abs]
-     [s/event-end] [is-eq abs]
-     [s/duration] [is-eq lcs lev shortest]
-     [s/frequency] [is-eq lcs lev shortest]
-     [s/website] [overlap]
-     [s/event-features s/s-name] [overlap lcs lev len-and-diff]
-     [s/event-org s/s-name] [overlap lcs lev len-and-diff]]]
+    [[s/stop-time] [is-eq lcs lev shortest]]
+    [[s/event-begin] [is-eq abs]]
+    [[s/event-end] [is-eq abs]]
+    [[s/duration] [is-eq lcs lev shortest]]
+    [[s/frequency] [is-eq lcs lev shortest]]
+    [[s/website] [overlap]]
+    [[s/event-features s/s-name] [overlap lcs lev len-and-diff]]
+    [[s/event-org s/s-name] [overlap lcs lev len-and-diff]]]
+   s/building
+   [[[s/street-addr] [is-eq lcs lev shortest]]
+    [[s/located-in s/zipcode] [is-eq lcs lev shortest]]
+    [[s/located-in s/s-name] [is-eq lcs lev shortest]]]
    s/email-body
    [[[s/email-body] [(count-regex #"\s+") (count-regex #">")
                      (count-regex #"\n|\r")
