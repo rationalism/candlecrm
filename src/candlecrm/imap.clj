9add29c94aa10247c3ef88d7a59b116a9c777be1
Alyssa Vance <alyssamvance@gmail.com>
2016-07-23 03:56:19 -0700

    fix concurrency security hole!

--- src/candlecrm/imap.clj
+++ src/candlecrm/imap.clj
@@ -137,23 +137,22 @@
       (google/delete-token! user) (auth/delete-queue! user)
       nil)))
 
-(defonce inbox (atom nil))
-
 ;; TODO: support IMAP stores other than GMail
 (defn fetch-imap-folder
   ([user] (fetch-imap-folder user false))
   ([user force-new]
-   (reset! inbox (if (and (not force-new)
-                          (contains? @imap-lookup user))
-                   (get @imap-lookup user)
-                   (refresh-inbox user)))
-   (when-not (folder-open? @inbox)
-     (try (open-folder-read! @inbox)
-          (catch Exception e
-            (do (reset! inbox (refresh-inbox user))
-                (open-folder-read! @inbox)))))
-   (update-imap-lookup! user @inbox)
-   @inbox))
+   (let [inbox (atom nil)]
+     (reset! inbox (if (and (not force-new)
+                            (contains? @imap-lookup user))
+                     (get @imap-lookup user)
+                     (refresh-inbox user)))
+     (when-not (folder-open? @inbox)
+       (try (open-folder-read! @inbox)
+            (catch Exception e
+              (do (reset! inbox (refresh-inbox user))
+                  (open-folder-read! @inbox)))))
+     (update-imap-lookup! user @inbox)
+     @inbox)))
 
 (defnp content [message]
   (.getContent message))
