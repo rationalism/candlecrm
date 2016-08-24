682ac95b3daaaf02208a82979b726ce0978a2e14
Alyssa Vance <alyssamvance@gmail.com>
2016-08-24 03:18:30 -0700

    add lev for emails (?)

--- src/candlecrm/model.clj
+++ src/candlecrm/model.clj
@@ -232,7 +232,7 @@
     [[s/email-uid] [is-eq which-nil]]]
    s/person
    [[[s/s-name] [overlap lcs lev-split len-and-diff]]
-    [[s/email-addr] [overlap is-eq shortest]]
+    [[s/email-addr] [overlap is-eq lev shortest]]
     [[s/link-to s/email-mentions s/email-sent] [is-eq]]]
    s/tool
    [[[s/tool-category] [is-eq]]
