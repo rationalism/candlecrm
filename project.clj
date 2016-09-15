e12b6d7ed15f76adbd9ba148abfe97aac1f9877d
Alyssa Vance <alyssamvance@gmail.com>
2016-09-15 00:16:31 -0700

    version update

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
                   :exclusions [com.google.code.findbugs/jsr305
                                com.google.guava/guava]]
                  [org.passay/passay "1.2.0"]
-                 [com.google.apis/google-api-services-oauth2 "v2-rev118-1.22.0"]
+                 [com.google.apis/google-api-services-oauth2 "v2-rev119-1.22.0"]
                  [com.google.http-client/google-http-client-jackson "1.22.0"]
                  [com.sun.mail/javax.mail "1.5.5"]
                  [de.jollyday/jollyday "0.4.7"]
