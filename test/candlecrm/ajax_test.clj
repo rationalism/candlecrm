710ac4468bfdc04168eab07450b85aa8c609e109
Alyssa Vance <alyssamvance@gmail.com>
2016-10-02 02:01:21 -0700

    ajax routing test

--- test/candlecrm/ajax_test.clj
+++ test/candlecrm/ajax_test.clj
@@ -1,15 +1,21 @@
 (ns candlecrm.ajax-test
   (:require [clojure.test :refer :all]
             [candlecrm.ajax :refer :all]
-            [candlecrm.neo4j :as neo4j]))
+            [candlecrm.auth :as auth]
+            [candlecrm.neo4j :as neo4j]
+            [candlecrm_cljc.schema :as s]))
 
 (defn graph-ready [f]
   (neo4j/graph-connect!)
   (neo4j/reset-session!)
+  (auth/load-keys!)
   (f)
   (neo4j/close-session!))
 
 (use-fixtures :once graph-ready)
+
+(def test-username "someemail@foo.com")
+(def test-password "notarealpassword")
 
 (deftest router-broadcaster
   (testing "Start and stop of Sente server"
@@ -17,3 +23,16 @@
     (start-broadcaster!)
     (is chsk-send!)
     (stop-router!)))
+
+(deftest routing-test
+  (testing "Ajax routing functions"
+    (def test-user (auth/create-user! {:username test-username
+                                       :password test-password}))
+
+    (is (= test-username
+           (s/email-addr (event-msg-handler* {:event nil :id :update/user-data
+                                              :identity test-user :?data []
+                                              :ring-req {:identity test-user}
+                                              :?reply-fn identity :send-fn nil}))))
+    
+    (auth/delete-user! test-user)))
