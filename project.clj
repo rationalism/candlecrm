c009f3eb6124f92c2998787fc0cabaec024ab87c
Alyssa Vance <alyssamvance@gmail.com>
2016-10-03 22:58:09 -0700

    version update

--- project.clj
+++ project.clj
@@ -66,7 +66,7 @@
                  [clojure-csv/clojure-csv "2.0.2"]
                  [org.apache.commons/commons-lang3 "3.4"]
                  [com.sendgrid/sendgrid-java "3.0.9"]
-                 [reagent-forms "0.5.25"
+                 [reagent-forms "0.5.26"
                   :exclusions [com.google.guava/guava]]
                  [org.apache.commons/commons-math3 "3.6.1"]
                  [org.bitbucket.cowwoc/diff-match-patch "1.1"]
