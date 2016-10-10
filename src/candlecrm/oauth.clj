3643fd663ba748969379ecce70185d8957dac516
Alyssa Vance <alyssamvance@gmail.com>
2016-10-10 04:18:44 -0700

    try outlook auth code

--- src/candlecrm/oauth.clj
+++ src/candlecrm/oauth.clj
@@ -2,5 +2,20 @@
   (:require [clojure.string :as str]
             [candlecrm.common :refer :all]
             [candlecrm_cljc.schema :as s]
-            [candlecrm.environ :refer [env]]))
+            [candlecrm.environ :refer [env]])
+  (:import [com.github.scribejava.core.builder ServiceBuilder]
+           [com.github.scribejava.apis LiveApi]))
 
+(def outlook-scope
+  "wl.imap wl.offline_access")
+
+(def outlook-callback
+  "/outlookauthcallback")
+
+(defn outlook-service []
+  (-> (ServiceBuilder. )
+      (.apiKey (env :outlook-client-id))
+      (.apiSecret (env :outlook-client-secret))
+      (.scope outlook-scope)
+      (.callback (full-callback-url outlook-callback))
+      (.build LiveApi/instance)))
