c91c364701ffb41435ef9bf10e305152b70f4581
Alyssa Vance <alyssamvance@gmail.com>
2016-10-13 17:09:37 -0700

    test fix

--- test/candlecrm/html_test.clj
+++ test/candlecrm/html_test.clj
@@ -6,7 +6,7 @@
   (testing "Load HTML forms, make sure they aren't null"
     (is (base-template "foobar"))
     (is (app-template "foobar"))
-    (is (login-form))
+    (is (login-form ""))
     (is (email-setup "" "foobar"))
     (is (signup-form "foobar"))))
 
